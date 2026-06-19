5a7aee05e468d5a5defe3c0a2d5dd682134948d0
devqcf <70290804+devqcf@users.noreply.github.com>
2026-06-19 06:06:28 +0500

    Initial Commit

---
+++ Usage StatusTests/Usage_StatusTests.swift
@@ -0,0 +1,19 @@
+//
+//  Usage_StatusTests.swift
+//  Usage StatusTests
+//
+//  Created by deargo on 19.06.2026.
+//
+
+import Testing
+@testable import Usage_Status
+
+struct Usage_StatusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
